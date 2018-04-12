034c3d18bb08bacea27be1f782ed5fcda7fb5b44
Xander Deseyn <xander.deseyn@gmail.com>
2018-04-12 15:32:42 +0200

    Fixed package setup & build steps

--- index.js
+++
@@ -1,4 +0,0 @@
-import CardGroup from './src/CardGroup';
-import OddsCalculator from './src/OddsCalculator';
-
-export { CardGroup, OddsCalculator };
---
+++ src/index.js
@@ -0,0 +1,4 @@
+import CardGroup from './CardGroup';
+import OddsCalculator from './OddsCalculator';
+
+export { CardGroup, OddsCalculator };
